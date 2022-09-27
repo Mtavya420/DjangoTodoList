9ad13e97182d1f3e46d858eacc088be32e1d8802
Dullah Mtavya <mtavya420@gmail.com>
2022-09-27 10:47:18 +0900

    made a some changes

--- todo_list/settings/prod.py
+++ todo_list/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -7,3 +8,7 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['todolist42.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
